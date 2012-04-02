e88b0aa5d32d3fb677641add50f507094485ff32
Ishai Jaffe <ishai@empeeric.com>
2012-04-02 18:45:18 +0300

    text field

--- models.js
+++ models.js
@@ -130,7 +130,7 @@ var Schemas = exports.Schemas = {
         title: {type: String, required: true},
         subject_id:{type:[ObjectId], ref:'Subject', index:true, required:true},
         category:{type:String, "enum":['test', 'statistics', 'infographic', 'graph'], required:true},
-        text_field:String,
+        text_field:{type:mongoose_types.Text},
         image_field: mongoose_types.File,
         tags:{type:[String], index:true},
         users:{type:[ObjectId], ref:'User',editable:false},
