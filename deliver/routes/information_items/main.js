81a70268183837268a388d892dfce91a8d64e65f
ishai <ishai@empeeric.com>
2012-06-07 12:33:42 +0300

    populate subject put in tag_name

--- deliver/routes/information_items/main.js
+++ deliver/routes/information_items/main.js
@@ -5,7 +5,7 @@ module.exports = function(req,res)
 {
     var item_id = req.params[0];
 
-    models.InformationItem.findById(item_id,function(err,item){
+    models.InformationItem.findById(item_id).populate('subject_id').run(function(err,item){
         if(err)
             res.render('500.ejs',{err:err});
         else
@@ -17,6 +17,7 @@ module.exports = function(req,res)
                 res.render('information_item.ejs',{
                     url: req.url,
                     item:item,
+                    tag_name:req.query.tag_name||'',
                     layout: false,
                     user_logged: req.isAuthenticated(),
                     user: req.session.user,
