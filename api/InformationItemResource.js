afdf32a064271a32c1f2324a6b259fd0dde1e87e
Ishai Jaffe <ishai@empeeric.com>
2012-06-10 13:25:58 +0300

    information item for unlogged users. + user_likes fix

--- api/InformationItemResource.js
+++ api/InformationItemResource.js
@@ -80,13 +80,6 @@ var InformationItemResource = module.exports = common.GamificationMongooseResour
 
     },
 
-    get_objects: function (req, filters, sorts, limit, offset, callback) {
-
-        this._super(req, filters, sorts, limit, offset, function(err, results){
-            callback(err, results);
-        });
-    },
-
     create_obj: function(req,fields,callback){
         var user_id = req.session.user_id;
         var self = this;
--- deliver/routes/information_items/main.js
+++ deliver/routes/information_items/main.js
@@ -15,25 +15,35 @@ module.exports = function(req,res)
                 res.redirect('/information_items');
             else
             {
-                var resource = new InformationItemResource();
-                resource.add_user_likes(req.session.user.id,item,function(err,item){
-                    if(err)
-                        res.render('500.ejs',{err:err});
-                    else
-                        res.render('information_item.ejs',{
-                            url: req.url,
-                            item:item,
-                            tag_name:req.query.tag_name||'',
-                            layout: false,
-                            user_logged: req.isAuthenticated(),
-                            user: req.session.user,
-                            auth_user: req.session.auth.user,
-                            tab:'information_items',
-                            avatar_url: req.session.avatar_url
-                        });
-                });
+                if(req.isAuthenticated())
+                {
+                    var resource = new InformationItemResource();
+                    resource.add_user_likes(req.session.user._id,item,function(err,item){
+                        console.log(item.user_like);
+                        if(err)
+                            res.render('500.ejs',{err:err});
+                        else
+                            render_information_item_page(req,res,item);
+                    });
+                }
+                else
+                    render_information_item_page(req,res,item);
             }
         }
     })
 
 };
+
+function render_information_item_page(req,res,item) {
+    res.render('information_item.ejs',{
+        url: req.url,
+        item:item,
+        tag_name:req.query.tag_name||'',
+        layout: false,
+        user_logged: req.isAuthenticated(),
+        user: req.session.user,
+        auth_user: req.session.auth.user,
+        tab:'information_items',
+        avatar_url: req.session.avatar_url
+    });
+}
