ea87fa2fd63b98b192a45f76703c71eca89e1284
Ishai Jaffe <ishai@empeeric.com>
2012-06-18 18:31:25 +0300

    added information by subject page

--- deliver/routes/information_items/index.js
+++ deliver/routes/information_items/index.js
@@ -3,6 +3,8 @@ module.exports = function(router)
 {
     router.all('',require('./list'));
 
+  //  router.all(/\/subject\/([0-9a-f]+)\/?$/,require('./subject'));
+
     router.all(/\/([0-9a-f]+)\/?$/,require('./main'));
 
 };
