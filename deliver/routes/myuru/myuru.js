5b28eab870aad63cf7543f6c08ba2ca90f1f3725
avnerkaf <avner@empeeric.com>
2012-06-24 14:29:59 +0300

    ש

--- deliver/routes/myuru/myuru.js
+++ deliver/routes/myuru/myuru.js
@@ -1,75 +1,97 @@
-
 var models = require('../../../models');
+var async = require('async');
 
-module.exports = function(req,res)
-{
+module.exports = function (req, res) {
 
-    function TokensBarModel(tokenPixels, numExtraTokens ,tokens,proxy){
+    function TokensBarModel(tokenPixels, numExtraTokens, tokens, proxy) {
 
-        this.proxy=proxy;
-       this.totalProxy=proxy.sum;// blue;
-        var dailyTokens = 9 +numExtraTokens;
+        this.proxy = proxy;
+        this.totalProxy = proxy.sum;// blue;
+        var dailyTokens = 9 + numExtraTokens;
         this.floorDailyTokens = Math.floor(dailyTokens);
 
-        this.gupFromFull=  15-  this.floorDailyTokens; //light gray
-        this.floorTokens=Math.floor(tokens); //green
-        this.tokensIUsed=  this.floorDailyTokens-this.floorTokens-this.totalProxy //dark gray
+        this.gupFromFull = 15 - this.floorDailyTokens; //light gray
+        this.floorTokens = Math.floor(tokens); //green
+        this.tokensIUsed = this.floorDailyTokens - this.floorTokens - this.totalProxy //dark gray
 
 
-      //  var  availableTokens=this.dailyTokens-this.totalProxy-user.tokens;
+        //  var  availableTokens=this.dailyTokens-this.totalProxy-user.tokens;
 
-       this.convertToPixels=  function (num){
-          return (num*tokenPixels)+'px';
+        this.convertToPixels = function (num) {
+            return (num * tokenPixels) + 'px';
         }
-        this.gupFromFullPixels=function (){ //light gray
-          return this.convertToPixels(this.gupFromFull)  ;
+        this.gupFromFullPixels = function () { //light gray
+            return this.convertToPixels(this.gupFromFull);
         }
-        this.dailyTokensPixels=function (){ //dark  gray
-            return this. convertToPixels(this.tokensIUsed)  ;
+        this.dailyTokensPixels = function () { //dark  gray
+            return this.convertToPixels(this.tokensIUsed);
         }
-        this.tokensPixels=function (){ //green
-            return this.convertToPixels(this.floorTokens)  ;
+        this.tokensPixels = function () { //green
+            return this.convertToPixels(this.floorTokens);
         }
-        this.ProxyPixels=function (){ //blue
-            return this.convertToPixels(this.totalProxy)  ;
+        this.ProxyPixels = function () { //blue
+            return this.convertToPixels(this.totalProxy);
         }
 
     }
 
 
-    var user= req.session.user  ;
-    models.User.findById(req.session.user._id, ["tokens", "num_of_extra_tokens", "proxy"], function(err, user_obj){
+    var user = req.session.user;
 
-        var dummyProxy={
+    async.waterfall([
+        function (cbk) {
+            models.User.findById(req.session.user._id, ["tokens", "num_of_extra_tokens", "proxy"], function(err, user){
+                cbk(err, user);
+            })
+        },
+
+        function (user_obj, cbk) {
+            if(!user_obj.proxy){
+                user_obj.proxy = [];
+            }
+            async.forEach(user_obj.proxy, function (proxy_user, itr_cbk) {
+                models.User.findById(proxy_user.user_id, ["_id", "first_name", "last_name", "facebook_id", "avatar"], function (err, curr_proxy_user) {
+                    if (!err) {
+                        curr_proxy_user.avatar = curr_proxy_user.avatar_url();
+                        proxy_user.user_id = curr_proxy_user;
+                    }
+                    itr_cbk();
+                })
+            }, function (err, ocj) {
+                cbk(err, user_obj);
+            })
+        }
+
+    ], function (err, user_obj) {
+        var dummyProxy = {
             sum:5,
             proxies:[
-                {name:'avner',proxy:2},
-                {name:'moty',proxy:1},
-                {name:'ron',proxy:2}
+                {name:'avner', proxy:2, _id:1},
+                {name:'moty', proxy:1, _id:2},
+                {name:'ron', proxy:2, _id:3}
             ]
-
         }
-       var   num_of_extra_tokens= 2//user_obj.num_of_extra_tokens ;
-        var  tokens=4 // user_obj.tokens;
-        var proxy=user_obj.proxy;
-        var tokensBarModel= new TokensBarModel(9,num_of_extra_tokens, tokens, dummyProxy);
+
+        var num_of_extra_tokens = 2//user_obj.num_of_extra_tokens ;
+        var tokens = 4 // user_obj.tokens;
+        var proxy = user_obj.proxy;
+        var tokensBarModel = new TokensBarModel(9, num_of_extra_tokens, tokens, dummyProxy);
         res.render('my_uru.ejs',
             {
-                layout: false,
+                layout:false,
                 tag_name:req.query.tag_name,
 
                 title:"אורו שלי",
-                logged: req.isAuthenticated(),
-                big_impressive_title: "",
-                user: user,
+                logged:req.isAuthenticated(),
+                big_impressive_title:"",
+                user:user,
                 avatar:req.session.avatar_url,
-                user_logged: req.isAuthenticated(),
+                user_logged:req.isAuthenticated(),
                 url:req.url,
                 tokensBarModel:tokensBarModel,
                 tab:''
             });
-    });
-
+    })
 };
 
 
