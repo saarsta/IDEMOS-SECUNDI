27fbf56ac5e966542a4807fd957729d3857ff03f
Ishai Jaffe <ishai@empeeric.com>
2012-06-25 20:46:04 +0300

    image in store, search without tags

--- api/ItemsCountByTagNameResource.js
+++ api/ItemsCountByTagNameResource.js
@@ -14,7 +14,9 @@ var ItemsByTagResource = module.exports = jest.Resource.extend({
             }
         };
         this.authentication = new common.SessionAuthentication();
-        this.filtering = {};
+        this.filtering = {
+            tag_name:null
+        };
         this.sorting = {};
         this.fields = {
             _id: null,
@@ -27,32 +29,36 @@ var ItemsByTagResource = module.exports = jest.Resource.extend({
     },
     get_objects:function(req,filters,sorts,limit,offset,callback)
     {
-        var tag_name = req.query.tag_name;
+        var tag_name = filters.tag_name;
         var cycles_count;
         var discussions_count;
         var info_items_count;
         var actions_count;
         var blogs_count;
 
+        var query = {};
+        if(tag_name)
+            query['tags'] = tag_name;
+
         async.parallel([
             function(cbk){
-                models.Cycle.count({tags: tag_name},cbk);
+                models.Cycle.count(query,cbk);
             },
 
             function(cbk){
-                models.Discussion.count({tags: tag_name}, cbk);
+                models.Discussion.count(query, cbk);
             },
 
             function(cbk){
-                models.InformationItem.count({tags: tag_name, status: "approved", is_visible: true}, cbk);
+                models.InformationItem.count(_.extend({status: "approved", is_visible: true},query), cbk);
             },
 
             function(cbk){
-                models.Action.count({tags: tag_name},cbk);
+                models.Action.count(query,cbk);
             },
 
             function(cbk){
-                models.Article.count({tags: tag_name}, cbk);
+                models.Article.count(query, cbk);
             }
 
         ], function(err, args){
