a61b03d06597f5c89e445ad5d932450aa61c226b
Saar <saar@empeeric.com>
2012-07-22 18:16:44 +0300

    fds

--- api/PostArticleResource.js
+++ api/PostArticleResource.js
@@ -17,6 +17,11 @@ var PostArticleResource = module.exports = common.GamificationMongooseResource.e
         this.default_query = function (query) {
             return query.sort('creation_date', 'ascending');
         };
+        this.update_fields = {
+            article_id: null,
+            text: null,
+            ref_to_post_id: null
+        },
         this.fields = {
             creator_id : common.user_public_fields,
             mandates_curr_user_gave_creator: null,
@@ -32,12 +37,42 @@ var PostArticleResource = module.exports = common.GamificationMongooseResource.e
             discussion_id:null,
             is_user_follower: null
         };
-//    this.validation = new resources.Validation();=
     },
 
-    run_query: function(req,query,callback)
+    run_query: function(req, query,callback)
     {
         query.populate('creator_id');
         this._super(req,query,callback);
+    },
+
+    create_obj:function (req, fields, callback) {
+
+        var creator_id = req.user._id;
+        var self = this;
+        var base = this._super;
+
+        /*
+        *  1 - set post fields and create post
+        *  final - return the object with creator user object
+        *
+        *
+        * */
+        async.waterfall([
+            //set post fields and create post
+            function(cbk){
+                fields.creator_id = user_id;
+                fields.first_name = user.first_name;
+                fields.last_name = user.last_name;
+                fields.avatar = user.avatar;
+                fields.ref_to_post_id = fields.ref_to_post_id || null;
+
+                base.call(self, req, fields, cbk);
+            }
+
+
+        ], function(err, post){
+            post.creator_id = req.user;
+            callback(err, post);
+        })
     }
 })
