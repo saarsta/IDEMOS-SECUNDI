0326d467d9d2f8d251dd3785264d02ea87532776
Ishai Jaffe <ishai@empeeric.com>
2012-08-01 11:40:26 +0300

    no description on caption

--- deliver/public/js/fb.js
+++ deliver/public/js/fb.js
@@ -16,12 +16,11 @@ function sendFacebookShare(link, title, src, text_preview, callback) {
         if(err)
             callback(err);
         else {
-            text_preview = text_preview.replace(/(<([^>]+)>)/ig,"");
+            text_preview = text_preview.replace(/(<([^>]+?)>)/ig,"");
             link =  window.location.protocol + '//' + window.location.hostname + link_obj.link;
             if (src.indexOf("http") == -1)
                 src = /*"http://uru.s3.amazonaws.com/spivak_29.jpg"*/window.location.protocol + '//' + window.location.hostname + src;
 
-            console.log(src);
 //            src = 'http://fbrell.com/f8.jpg';
 
             FB.ui({
@@ -29,8 +28,8 @@ function sendFacebookShare(link, title, src, text_preview, callback) {
                 link: link,
                 name: title,
                 picture: src,
-                caption: text_preview
-//                description: text_preview
+                caption: text_preview,
+                description: ' '
             }, function(response) {
                 console.log(response);
                 callback();
