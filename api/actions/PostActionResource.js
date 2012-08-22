b91ffaaeb80f07811729a205ded18d091c363a30
Saar <saar@empeeric.com>
2012-08-22 12:00:47 +0300

    createActionPost - put user in creator_id

--- api/actions/PostActionResource.js
+++ api/actions/PostActionResource.js
@@ -149,6 +149,11 @@ var PostActionResource = module.exports = common.GamificationMongooseResource.ex
             }
         ],function(err,result)
         {
+            post_object.is_user_follower = false;
+            //update each post creator with his vote balance
+            post_object.voter_balance = 0;
+            post_object = JSON.parse(JSON.stringify(post_object));
+            post_object.creator_id = req.user;
             callback(err, post_object);
         });
     }
