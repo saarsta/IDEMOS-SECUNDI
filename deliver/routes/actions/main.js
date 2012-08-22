786ad89a1f946b8b24492395b0762003b964b10c
Saar <saar@empeeric.com>
2012-08-22 15:17:59 +0300

    action going users - models.join.find

--- deliver/routes/actions/main.js
+++ deliver/routes/actions/main.js
@@ -1,20 +1,19 @@
-
 var models = require('../../../models'),
     async = require('async');
 
-module.exports = function(req, res){
+module.exports = function (req, res) {
 
 
     /*
-    * 1. get action
-    * 2. get "going_users" of action
-    *
-    *
-    *
-    * */
+     * 1. get action
+     * 2. get "going_users" of action
+     *
+     *
+     *
+     * */
 
-     async.parallel([
-        function(cbk){
+    async.parallel([
+        function (cbk) {
             models.Action.findById(req.params[0])
                 .select({
                     '_id':1,
@@ -32,45 +31,57 @@ module.exports = function(req, res){
         },
 
         // get the user object
-        function(cbk) {
-            if(req.session.user)
+        function (cbk) {
+            if (req.session.user)
                 models.User.findById(req.session.user._id, cbk);
-            else
-            {
+            else {
                 cbk(null, null);
             }
+        },
+
+        function (cbk) {
+            models.Join.find({action_id: req.params[0]})
+                .select({_id:1})
+                .exec(cbk);
         }
-    ], function(err, args){
+    ], function (err, args) {
 
 
         var action = args[0];
-        var proxyJson=  args[1] ?  JSON.stringify(args[1].proxy) : null;
-        if(err)
-            res.render('500.ejs',{error:err});
+        var proxyJson = args[1] ? JSON.stringify(args[1].proxy) : null;
+        var going_users = args[2];
+
+
+
+
+        if (err)
+            res.render('500.ejs', {error:err});
         else {
 
-            if(!action)
+            if (!action)
                 res.render('404.ejs');
             else {
-                action.location=
+                action.location =
                     'התעשייה 12, תל אביב';
-                action.from_date=action.execution_date;
-                action.to_date=action.execution_date;
+                action.from_date = action.execution_date;
+                action.to_date = action.execution_date;
                 var is_going = false;
-               // is user going to action?
-               if(req.user){
-                   var user_id = req.user._id;
-                   is_going = _.any(going_users, function(going_user){ going_user._id + "" == user_id})
-               }
-               action.is_going = is_going;
+                // is user going to action?
+                if (req.user) {
+                    var user_id = req.user._id;
+                    is_going = _.any(going_users, function (going_user) {
+                        going_user._id + "" == user_id
+                    })
+                }
+                action.is_going = is_going;
 
-                var ejsFileName=true?'action_approved.ejs':'action_append.ejs';
-                res.render(ejsFileName,{
-                    action: action,
-                    tab: 'actions',
+                var ejsFileName = true ? 'action_approved.ejs' : 'action_append.ejs';
+                res.render(ejsFileName, {
+                    action:action,
+                    tab:'actions',
                     proxy:proxyJson
 
-                   // pageType:'beforeJoin' //waitAction,beforeJoin
+                    // pageType:'beforeJoin' //waitAction,beforeJoin
 
 
                 });
--- deliver/routes/elections/fbimage.js
+++ deliver/routes/elections/fbimage.js
@@ -5,32 +5,7 @@ var async = require("async");
 
 
 module.exports = function(req, res) {
-//    async.waterfall([
-//        // get the user by id
-//        function (cb) {
-//            models.User.findById(req.params.id, cb)
-//        },
-//        // get assosiated discussions
-//        function (user, cb)
-//        {
-//            // we might have some placeholders in this list
-//            var disc_ids = user.has_voted.filter(function(val) {return val.length > 20;});
-//            models.Discussion.find({_id: {'$in': disc_ids}}, {title:1, text_field_preview:1}, cb)
-//        }
-//    ], function(err, discussion){
-//        if (err) {
-//            res.send(500, err);
-//            return;
-//        }
-//        var items = discussion.map(function(dis) {
-//            return {title: dis.title, text: dis.text_field_preview};
-//        });
-//        res.render('fbimage.ejs', {
-//            layout: false,
-//            url:req.url,
-//            items:items
-//        });
-//    });
+
     var data = {};
     data.user_id = req.params.id;
 
@@ -63,9 +38,6 @@ var getUserChosenDiscussions = module.exports.getUserChosenDiscussions = functio
         }
     ], function(err, discussions){
         if(!err)
-//            var items = discussion.map(function(dis) {
-//                return {title: dis.title, text: dis.text_field_preview};
-//            });
 
         callback(err, discussions);
     });
