00c7e350d2596a2b96bff361ddef9a77e7b31826
Ishai Jaffe <ishai@empeeric.com>
2012-08-23 10:35:20 +0300

    change in site meta description & title

--- app.js
+++ app.js
@@ -126,11 +126,35 @@ app.configure('production', function(){
 
     app.set('send_mails',true);
 
+    process.on('uncaughtException', function(err) {
+        console.trace(err);
+    });
+
 });
 
 
 if(!mongoose.connection.host)
     mongoose.connect(app.settings.DB_URL);
+
+mongoose.connection.on('error',function(err) {
+    console.error('db connection error: ',err);
+});
+
+mongoose.connection.on('disconnected',function(err){
+    console.error('DB disconnected',err);
+    var reconnect = function(){
+        mongoose.connect(app.settings.DB_URL,function(err) {
+            if(err)
+                console.error(err);
+            else {
+                session_middleware = express.session({secret: confdb.secret,
+                    maxAge: new Date(Date.now() + (3600 * 1000 * 24)),
+                    store: new MongoStore(confdb.db) });
+            }
+        });
+    };
+    setTimeout(reconnect,200);
+});
 
 var models = require('./models');
 models.setDefaultPublish(app.settings.show_only_published);
@@ -140,8 +164,11 @@ var account = require('./deliver/routes/account');
 // Configuration
 var confdb = {
     db: utils.split_db_url(app.settings.DB_URL),
-    secret: '076ed61d63ea10a12rea879l1ve433s9'
+    secret: '076ed61d63ea10a12rea879l1ve433s9',
+    auto_reconnect:true
 };
+
+var session_middleware;
 
 app.configure(function(){
     utils.setShowOnlyPublished(app.settings.show_only_published);
@@ -179,9 +206,14 @@ app.configure(function(){
 
     app.set('view options', { layout: false });
 
-    app.use(express.session({secret: confdb.secret,
+    session_middleware = express.session({secret: confdb.secret,
         maxAge: new Date(Date.now() + (3600 * 1000 * 24)),
-        store: new MongoStore(confdb.db) }));
+        store: new MongoStore(confdb.db) });
+
+    app.use(function(req,res,next) {
+        session_middleware(req,res,next);
+    });
+
     app.use(account.referred_by_middleware);
 
     var fbId = app.settings.facebook_app_id,
@@ -222,7 +254,11 @@ app.configure(function(){
 
     app.locals({
         footer_links:function() { return mongoose.model('FooterLink').getFooterLinks(); },
-        cleanHtml:function(html) { return html.replace(/<[^>]*?>/g,'').replace(/\[[^\]]*?]/g,'');}
+        cleanHtml:function(html) { return (html || '').replace(/<[^>]*?>/g,'').replace(/\[[^\]]*?]/g,'');},
+        fb_description:"עורו היא תנועה חברתית לייצוג הרוב בישראל. אנו מאמינים שבעידן שבו אנו חיים, כולנו מסוגלים וזכאים להשתתף בקבלת ההחלטות. לכן, עורו מנהלת פלטפורמה לדיון ציבורי, יסודי ואפקטיבי שיוביל שינוי בסדר היום. אצלנו, האג'נדה מוכתבת מלמטה.",
+        fb_title:'עורו - הבית של הרוב',
+        fb_image:'http://site.e-dologic.co.il/philip_morris/Xls_script/uru_mailing/logo.jpg'
+
     });
 
 
@@ -231,9 +267,6 @@ app.configure(function(){
 
 });
 
-process.on('uncaughtException', function(err) {
-    console.error(err);
-});
 
 //if(app.settings.env != 'production')
 //require('./routes')(app);
