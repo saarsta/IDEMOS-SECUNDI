06cbfd7c79605a94b8a56f574977145ab4229675
Refael Ackermann <refack@gmail.com>
2012-08-30 14:25:57 +0300

    More discussions

--- deliver/routes/elections/fbimage.js
+++ deliver/routes/elections/fbimage.js
@@ -59,8 +59,9 @@ var getUserChosenDiscussions = module.exports.getUserChosenDiscussions = functio
             // we might have some placeholders in this list
 
             var disc_ids = user.has_voted.filter(function(val) {return val.length > 20;});
+            var stored_disc = user.has_voted.filter(function(val) {return val.title;});
             models.Discussion.find({_id: {'$in': disc_ids}}, function(err, result){
-                cb(err, result)
+                cb(err, result + stored_disc)
             })
         }
     ], callback
