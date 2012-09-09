2cbf1161a85878714b713a5f6d3eea4a1e82fd1d
Saar <saar@empeeric.com>
2012-09-09 18:40:27 +0300

    discussions -- when gice mandate -- if user is not logged login box popup  is shown

--- deliver/public/js/proxy_common.js
+++ deliver/public/js/proxy_common.js
@@ -46,7 +46,6 @@ var proxyCommon={
                 }
 
                 db_functions.addOrRemoveProxyMandate(my_id, proxyId, changeProxy, function (err, data) {
-                    debugger
                     //  var msg_params = err ? {massage:err.responseText} : {massage:"האסימונים ילקחו בקרוב"};
                     var fixMeProxy;
                     if(data && data.proxy){
