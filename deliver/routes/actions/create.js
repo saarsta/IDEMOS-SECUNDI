4e66737b5298be33b24f59d60b0bf5aa2b06aac5
Dor Kleiman <dor@empeeric.com>
2012-09-13 16:04:20 +0300

    Minor refactor of actions/create route.

--- deliver/routes/actions/create.js
+++ deliver/routes/actions/create.js
@@ -5,36 +5,28 @@ var models = require('../../../models'),
 module.exports = {
     get: function (req, res) {
         var id = req.params.cycle_id;
-        async.parallel([
-            function (callback) {
+        async.parallel({
+            cycle: function (callback) {
                 models.Cycle.findById(id, callback);
             },
-            function (callback) {
-                models.ActionResource.find({}, callback);
+            resources: function (callback) {
+                models.ActionResource.find({ is_approved: true }, callback);
             },
-            function (callback) {
+            categories: function (callback) {
                 models.Category.find({}, callback);
             }
-        ], function (err, results) {
-            var cycle = results[0],
-                resources = results[1],
-                categories = results[2];
-
-            if (!cycle) {
+        }, function (err, results) {
+            if (!results.cycle) {
                 console.log('Cycle id ' + id + ' not found; returning 404.');
                 res.render('404.ejs');
                 return;
             }
 
-            console.log('create action for cycle id ' + id + ': ' + cycle.title);
-
-            var today = utils.dateFormat('yyyy-mm-dd');
-
             res.render('action_create.ejs', {
-                cycle: cycle,
-                resources: resources,
-                categories: categories,
-                today: today
+                cycle: results.cycle,
+                resources: results.resources,
+                categories: results.categories,
+                today: utils.dateFormat('yyyy-mm-dd')
             });
         });
     }
