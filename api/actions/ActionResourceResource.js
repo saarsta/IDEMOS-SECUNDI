e5b4ee97fe2a308abe30118a3a8a9b603bab4245
Dor Kleiman <dor@empeeric.com>
2012-09-13 16:04:53 +0300

    Only create an action resource when posting on /api/action_resource. Also, default is_approved = false.

--- api/actions/ActionResourceResource.js
+++ api/actions/ActionResourceResource.js
@@ -15,49 +15,13 @@ var ActionResourceResource = module.exports = common.GamificationMongooseResourc
     },
 
     create_obj: function(req, fields, callback){
+        var object = new this.model();
 
-        var self = this;
-        var object = new self.model();
-        var action_id = req.body.action_id;
-        var amount = req.body.amount;
-        var g_action_resource;
-        var g_action;
+        fields.is_approved = false;
+        for (var field in fields) {
+            object.set(field, fields[field]);
+        }
 
-        async.waterfall([
-
-            function(cbk){
-                models.Action.findById(action_id, cbk);
-            },
-
-            function(action, cbk){
-                g_action = action;
-
-                fields.is_approved = false;
-                //all action resources of action have the same category..
-                fields.category = action.action_resources[0].category;
-
-                for (var field in fields) {
-                    object.set(field, fields[field]);
-                }
-
-                self._super(req, fields, cbk);
-            },
-
-            function(cbk, action_resource){
-
-                var new_action_resource = {
-                    resource : action_resource._id,
-                    amount: amount,
-                    left_to_bring: amount
-                }
-
-                g_action.action_resources.push(new_action_resource);
-                g_action.save(function(err, action){ cbk(err, action) })
-            }
-        ], function(err, hi){
-            callback(err, g_action_resource);
-        })
+        this._super(req, fields, callback);
     }
-})
-
-
+});
