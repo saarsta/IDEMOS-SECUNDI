dd33a8648a83020ddde4fe257ef1765a396ab7ff
Saar <saar@empeeric.com>
2012-09-19 15:15:19 +0300

    https://www.gravitydev.com/project/46603/issues/460 לחיצה על על "הראה לי את כל הפעולות הממתינות לאישור" בעמוד מעגל

--- deliver/routes/actions/cycle.js
+++ deliver/routes/actions/cycle.js
@@ -2,17 +2,8 @@ var models = require('../../../models');
 
 module.exports = function(req,res)
 {
-    res.render('actions_list.ejs'),
-        {
-            layout: false,
-//            tag_name:req.query.tag_name,
-//            title:"פעולות",
-//            logged: req.isAuthenticated(),
-//            big_impressive_title: "",
-
-//            url:req.url,
-//            tab:'cycles'
-            user_logged: req.isAuthenticated(),
+    console.log(req.params[0]);
+    res.render('actions_list.ejs', {
             cycle_id: req.params[0]
-        };
+        });
 };
