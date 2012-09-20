9bb8956ab9da55611637e1707513a47d4b4b1c79
Saar <saar@empeeric.com>
2012-09-20 12:14:16 +0300

    https://www.gravitydev.com/project/46603/issues/465

--- api/UpdateResource.js
+++ api/UpdateResource.js
@@ -16,7 +16,7 @@ var UpdateResource = module.exports = jest.MongooseResource.extend(
             this._super(models.Update, null, null);
             this.allowed_methods = ['get'];
             this.authentication = new common.SessionAuthentication();
-            this.filtering = {cycles: null};
+            this.filtering = {cycle: null};
             this.default_query = function (query) {
                 return query.where('is_visible', true).sort({creation_date: 'descending'});
             };
