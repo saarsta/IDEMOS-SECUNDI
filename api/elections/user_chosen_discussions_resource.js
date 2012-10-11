ed75ccb633b2982cfd2862b5e4dd55a91eb5ed1f
Saar <saar@empeeric.com>
2012-10-11 14:28:54 +0200

    https://www.gravitydev.com/project/46603/issues/481 for now i put post_counter in grade_sum field cause mongoose don't allow me to add a field

--- api/elections/user_chosen_discussions_resource.js
+++ api/elections/user_chosen_discussions_resource.js
@@ -13,41 +13,6 @@ var UserChosenDiscussionsResource = module.exports =  jest.Resource.extend({
         this.update_fields = {
             user_id:null
         };
-        this.fields = {
-            title: null,
-            tooltip:null,
-//        text_field:{type:mongoose_types.Html},
-//        text_field_preview:{type:mongoose_types.Html},
-            image_field: null,
-            image_field_preview: null,
-            subject_id: null,
-            subject_name: null,
-            system_message: null,
-            creation_date: null,
-            creator_id: null,
-            first_name: null,
-            last_name:null,
-            text_field_preview: null,//2-3 lines of the vision_text
-            text_field: null,
-            vision_text_history: null,
-            num_of_approved_change_suggestions: null,
-            is_hot_object: null,
-            is_cycle: null,
-            tags: null,
-            //users that connected somehow to discussion for my uru
-            users: null,
-
-            //followers for my uru
-            followers: null,
-            view_counter: null,
-            followers_count: null,
-            is_visible: null,
-            is_published: null,
-            grade: null,
-            evaluate_counter: null,
-            grade_sum: null,
-            post_counter: null
-        }
     },
 
     get_object:getUserChosenDiscussions
