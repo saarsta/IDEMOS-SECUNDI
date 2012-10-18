37171fffe44870ab052879a187b5cf6b88b6142f
Lior Ur <liorur@gmail.com>
2012-10-18 11:54:00 +0200

    daily_discussion

--- deliver/routes/daily_discussions/main.js
+++ deliver/routes/daily_discussions/main.js
@@ -12,8 +12,8 @@ module.exports = function(req,res)
     async.parallel([
         function(cbk)  {
             models.DailyDiscussion.findById(req.params[0], cbk)
-         //   .populate('main_subject', {'name':1})
-         //   .populate('discussion_id',{'title':1});
+            .populate('subject', {'name':1})
+            .populate('discussion');
         }
     ],
         function(err, results)
@@ -27,15 +27,12 @@ module.exports = function(req,res)
                 else
                 {
                     var daily_discussion=results[0];
-                    var related_discussion= results[0].discussion_id;
                     res.setHeader("Expires", "0");
                     res.render('daily_discussion.ejs',{
                         title:"דיון",
                         daily_discussion_id: req.params[0],
-                        subject_id: req.query.subject_id,
                         tab:'discussions',
                         discussion: daily_discussion,
-                        related_discussion:  related_discussion,
                         fb_description: daily_discussion.text_field_preview,
                         fb_title: daily_discussion.title,
                         fb_image:daily_discussion.image_field && daily_discussion.image_field.url
