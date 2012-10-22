a6cbd887967ba33e3f6e1ccecedcc943da333305
avnerkaf <avner@empeeric.com>
2012-10-22 12:00:33 +0200

    553cycle - timeline\ מעגל:
    תהיה אפשרות לשים 2 אובייקטי טיימליין בדיפולט שהפופאפ שלהם יהיה פתוח בעליית הדף.
    הפופאפ ייסגר אחרי שהיוזר עובר על הפופאפ או שיוזר פותח פופאפ אחר.
    
    clusters dont work yet, partial fix

--- deliver/public/js/timeline.js
+++ deliver/public/js/timeline.js
@@ -169,6 +169,9 @@ var timeline= {
                 }
                 item.offset = offsetFromIndex(j);
                 item.timeline_index=i;
+                if(display_id){
+                    item.is_default_displayed = false;
+                }
                 switch(item.type)
                 {
                     case 'past':
@@ -206,6 +209,22 @@ var timeline= {
                 }
             });
 
+            if(display_id) {
+                $('.popup-event[item-id="'+display_id+'"]').addClass('showdiv')   ;
+            } else {
+                $('.icon').mouseenter(function() {
+                    var diagram = $('.followers-diagram');
+
+                    $.each(diagram.find('div'), function(index, element){
+                        if($(element).hasClass('showdiv'))
+                        {
+                            $(element).fadeOut(2000);
+                        }
+                    })
+
+                });
+            }
+
             $.each(render_clusters, function (index1, cluster_array) {
                 var it= [];
                 var dummy=true;
@@ -224,14 +243,9 @@ var timeline= {
                     dust.render('cycle_timeline_dummy', {date:index1,offset:offset}, timelineAppend);
                 }
                 animateCluster(it);
-                if(display_id)
-                {
-                    $('.popup-event[item-id="'+display_id+'"]').addClass('showdiv')   ;
-                    $('.popup-event[item-id="'+display_id+'"]').parent().mouseenter(function() {
-                        $('.popup-event[item-id="'+display_id+'"]').removeClass('showdiv') ;
-                    });
 
-                }
+
+
 
             });
         });
