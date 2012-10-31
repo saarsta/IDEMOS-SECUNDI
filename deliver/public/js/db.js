saarsta/IDEMOS-SECUNDI

commit 2ca9c1b48fb9bfcdb0b489b0ade5c47347b11b38

social popup update

deliver/public/js/db.js
@@ -538,7 +538,10 @@ var db_functions = {
             }
         });
     },
-    addFacebookRequest:function (link, request_ids, callback) {
+
+    addFacebookRequest:function (link, response, callback) {
+        var request_ids =response.request;
+        var to  =response.to;
         db_functions.loggedInAjax({
             url:'/api/fb_request/',
             type:"POST",
@@ -546,6 +549,7 @@ var db_functions = {
             contentType:'application/json',
             data:JSON.stringify({"link":link, "fb_request_ids":request_ids}),
             success:function (data) {
+                data.response=  response;
                 console.log(data);
                 callback(null, data);
             },
@@ -1442,6 +1446,23 @@ var db_functions = {
             },
             error:function (err) {
                 callback(err, null);
+            }
+        });
+    } ,
+
+
+    submitInvitedFriends: function (object_type,object_id,facebook_ids,emails, callback) {
+
+        db_functions.loggedInAjax({
+            url:'/api/user_invited_friends',
+            type:"POST",
+            async:true,
+            data:{type:object_type, id:object_id,email:emails,facebook:facebook_ids},
+            success:function (data, err) {
+                callback(err, data);
+            },
+            error:function (err, data) {
+                callback(err, data);
             }
         });
     }

api/UserInviteFriendsResource.js
@@ -1,0 +1,65 @@
+/**
+ * Created by
+ * User: liorur
+ * Date: 22/10/12
+ */
+
+var resources = require('jest'),
+    util = require('util'),
+    models = require('../models'),
+    common = require('./common'),
+    async = require('async'),
+    _ = require('underscore');
+
+
+var UserInviteFriendsResource = module.exports = common.GamificationMongooseResource.extend({
+    init: function(){
+        this._super(models.User, null, 0);
+        this.allowed_methods = ['post'];
+        this.authentication = new common.SessionAuthentication();
+        //returned
+        this.fields = {
+            _id: null   ,
+            invited_friends:null
+        }
+    },
+
+    create_obj: function(req,fields,callback){
+
+        models.User.findById(req.user._id, function(err,user){
+            //look for exisiting entry with same object id- if exist the unify else:
+
+            var new_invite_object  =  {
+                object_type:fields.type,
+                object_id:fields.id,
+                facebook_request:fields.facebook ?fields.facebook.request:null  ,
+                facebook_ids:fields.facebook ?fields.facebook.to : null  ,
+                emails:fields.email  ,
+                date: {type:Date, 'default':Date.now}
+            }
+
+             if(user.invited_friends){
+                 var invite_object =  _.find(user.invited_friends, function(invite){
+                     return  invite.object_id  == fields.id;
+                 });
+                 if(invite_object) {
+                     user.invited_friends  =        user.invited_friends.filter(function (invite, index, array) {
+                             return  invite.object_id  != fields.id;
+                     });
+                     new_invite_object.emails = _.union(new_invite_object.emails,new_invite_object.emails,invite_object.emails)
+
+                 }
+             }
+
+             user.invited_friends.push( new_invite_object);
+
+            user.save(function(err, obj){
+
+                callback(err, obj);
+            })
+
+        });
+
+
+    }
+})

api/index.js
@@ -63,9 +63,9 @@ var mongoose_resource = require('jest'),
     FbConnectResource = require('./FbConnectResource'),
     AvatarResource = require('./avatar_resource'),
     ResetNotificationResource = require('./reset_notification_resource'),
-    DailyDiscussionResource = require('./DailyDiscussionResource.js'),
-    OGActionResource = require('./og_action_resource');
-
+    DailyDiscussionResource = require('./DailyDiscussionResource'),
+    OGActionResource = require('./og_action_resource'),
+    UserInviteFriendsResource=      require('./UserInviteFriendsResource');
 
 
 module.exports = function(app)
@@ -116,6 +116,9 @@ module.exports = function(app)
     rest_api.register_resource('notifications', new NotificationResource());
     rest_api.register_resource('user_followers', new UserFollowerResource());
     rest_api.register_resource('user_proxies', new UserProxyResource());
+    rest_api.register_resource('user_proxies', new UserProxyResource());
+    rest_api.register_resource('user_invited_friends', new UserInviteFriendsResource());
+
     rest_api.register_resource('about_uru_texts', new AboutUruTextResource());
     rest_api.register_resource('about_uru_items', new AboutUruItemResource());
     rest_api.register_resource('team', new TeamResource());

deliver/public/css/style.css
@@ -8778,7 +8778,7 @@ div.icons img {
 }
 
 .sb-bottom {
-    height: 140px;
+    min-height: 140px;
     padding:  10px;
 
     background: #ededed;
@@ -8806,6 +8806,11 @@ div.icons img {
     height: 30px;
 }
 
+#cloudsponge{
+    margin-top: 30px;
+    display: none;
+
+}
 
 
 

deliver/public/js/fb.js
@@ -5,12 +5,11 @@ function sendFacebookInvite(message,link,callback) {
             if(!response) {
                 callback('canceled');
             } else {
-                db_functions.addFacebookRequest(link, response.request, callback);
+                db_functions.addFacebookRequest(link, response, callback);
             }
         }
     );
 }
-
 
 
 function sendFacebookShare(_, title, src, text_preview, callback) {

deliver/views/partials/head.ejs
@@ -64,7 +64,7 @@
 <script src="/js/date.format.js" type="text/javascript"></script>
 <script src="/js/jquery.easing.1.3.js" type="text/javascript"></script>
 <script src="/js/timeline.js" type="text/javascript"></script>
-
+<script src="https://api.cloudsponge.com/address_books.js" type="text/javascript"></script>
 
 <script src="/js/js-webshim/minified/extras/modernizr-custom.js"></script>
 <script src="/js/js-webshim/minified/polyfiller.js"></script>

deliver/views/partials/social_popup.ejs
@@ -29,20 +29,62 @@
                 <div class="sb-icons">
                     <div id="close"><img src="/images/elections/close.png" alt="עורו"/></div>
                     <div class="icons">
-                        <a href="mailto:?subject=%D7%92%D7%9D%20%D7%90%D7%A0%D7%99%20%D7%9E%D7%A8%D7%92%D7%99%D7%A9%20%D7%90%D7%96%D7%A8%D7%97%20%D7%9C%D7%90%20%D7%9E%D7%99%D7%95%D7%A6%D7%92%20%E2%80%93%20%D7%90%D7%96%20%D7%91%D7%97%D7%A8%D7%AA%D7%99%20%D7%91%D7%A0%D7%95%D7%A9%D7%90%D7%99%D7%9D%20%D7%94%D7%97%D7%A9%D7%95%D7%91%D7%99%D7%9D%20%D7%9C%D7%99%20%D7%91%D7%90%D7%9E%D7%AA!&body=מרגישים כמוני שהפוליטיקאים לא סופרים אותנו? במקום להתייאש הצטרפתי לעורו כדי שנוכל להתאגד ולקבוע סדר יום אזרחי חדש. הצבעתי בבחירות של עורו! http://uru.org.il"
-                           target="_blank"><img class="icon" src="/images/elections/enva.png" alt="עורו"/></a>
+                        <a href="#" id="mail" ><img class="icon" src="/images/elections/enva.png" alt="עורו"/></a>
                         <a href="javascript:void(0)" id="share-google"><img class="icon" src="/images/elections/google.png" alt="עורו"/></a>
                         <a href="javascript:void(0)" id="share-twitter"><img class="icon" src="/images/elections/tweet.png" alt="עורו"/></a>
                         <a href="javascript:void(0)" id="share-facebook"><img class="icon" src="/images/elections/face.png" alt="עורו"/></a>
                     </div>
                 </div>
             </div>
-            <div>
-                </br>
-                Some text
+            <div id="cloudsponge">
+                <!-- Include these scripts to import address books with CloudSponge -->
+
+                <script type="text/javascript" charset="utf-8">
+                    var csPageOptions = {
+
+                         domain_key:"MEJVM6QW5TG7SQF4B8G3",
+                       // domain_key:"G5XMTFDEGQNEND8A6YZB",
+                        textarea_id:"contact_list",
+                        include:['mailing_address','name','email'],
+                        afterSubmitContacts: function(contacts, source, owner) {
+
+
+                            var i, contact, name, email, phone, mailing_address ,emails=[];
+                            for (i = 0; i < contacts.length; i++) {
+                                contact = contacts[i];
+                                name = contact.fullName();
+                                email = contact.selectedEmail();
+
+                                console.log(email) ;
+                                emails.push(email);
+                            }
+                            db_functions.submitInvitedFriends('action',action_id,null,emails, function (data, err){
+
+                            } );
+                        }
+                    }
+                </script>
+
+                <!-- Any link with a class="cs_import" will start the import process -->
+                <div id="deep-link" style="">
+        <span id="" style="padding-right: 20px;">
+
+          <a id="source-yahoo" href="#" onclick="return cloudsponge.launch('yahoo');"><img alt="Yahoo" height="28" src="http://www.cloudsponge.com/images/address_books/yahoo.png?1349284367" width="28"></a>
+           <a id="source-windowslive" href="#" onclick="return cloudsponge.launch('windowslive');"><img alt="Windows Live" height="28" src="http://www.cloudsponge.com/images/address_books/msn.png?1349284367" width="28"></a>
+
+          <a id="source-outlook" href="#" onclick="return cloudsponge.launch('outlook');" style="display: inline; "><img alt="Microsoft Outlook" height="28" src="http://www.cloudsponge.com/images/address_books/microsoft.png?1349284367" width="28"></a>
+          <a id="source-addressbook" href="#" onclick="return cloudsponge.launch('addressbook');" style="display:none"><img alt="Mac Address Book" height="28" src="http://www.cloudsponge.com/images/address_books/apple.png?1349284367" width="28"></a>
+                        <a id="source-gmail" href="#" onclick="return cloudsponge.launch('gmail');"><img alt="Gmail" height="28" src="http://www.cloudsponge.com/images/address_books/google.png?1349284367" width="28"></a>
+
+        </span>
+                </div>
+                <!-- This textarea will be populated with the contacts returned by CloudSponge
+                <textarea id="contact_list" style="width:450px;height:82px"></textarea>
+                -->
             </div>
         </div>
     </div>
+
 </div>
 
 <div id="screen"></div>
@@ -142,13 +184,31 @@
                 + " "
                 + "אשמח לראותך שם!";
         }
-        sendFacebookShare(rel, title, img_src, text_preview, function(err){
-            if(err){
-                alert('Error occured');
-            }else{
-//                popupProvider.showOkPopup({message: 'השיתוף הסתיים בהצלחה.'});
+          /*
+         sendFacebookShare(rel, title, img_src, text_preview, function(err){
+         if(err){
+         alert('Error occured');
+         }else{
+         //                popupProvider.showOkPopup({message: 'השיתוף הסתיים בהצלחה.'});
+         }
+
+         });
+
+           sendFacebookInvite("test", "http://www.uru.org.il/actions/"+action_id,function(err, data){
+           'הזמנה להצטרף לפעולה!'
+         */
+
+
+        sendFacebookInvite('Come see that great site', "/",function(err, data){
+            if(!err)
+            {
+                popupProvider.showOkPopup({message:"ההזמנה הצליחה!"});
+                console.log(data);
+                db_functions.submitInvitedFriends('action',action_id,data.response,null);
             }
         });
+
+
     });
 
     $('#share-twitter').on('click', function () {
@@ -163,5 +223,11 @@
                 'share-google', 'status=0,scrollbars=0,location=0,height=320,width=660,resizable=1');
         return false;
     });
+
+    $('#mail').on('click', function () {
+        $("#cloudsponge").slideToggle();
+        return false;
+    });
+
 
 </script>

models/user.js
@@ -59,6 +59,17 @@ var User = module.exports = new Schema({
     followers: [
         new Schema({follower_id:{type:ObjectId, ref:'User',query:common.FIND_USER_QUERY}, join_date: {type:Date, 'default':Date.now}})
     ],
+    invited_friends: [
+        new Schema({
+            object_type:{type:String},
+            object_id:{type:ObjectId},
+            facebook_request:{type:String}  ,
+            facebook_ids:[{type:String}]  ,
+            emails:[{type:String}]  ,
+            date: {type:Date, 'default':Date.now}
+        })
+    ],
+
     password: {type: String, editable:false},
     validation_code: {type: String, editable:false},
     tokens:{type:Number, 'default':9, min: 0/*, max:15.9*/},

commit f26639c0429d34ee64eaa9518748649b69094029

FIX BUG : 616 (gravity) regression caused by commit 2ca9c1b48fb9bfcdb0b489b0ade5c47347b11b38

deliver/public/js/db.js
@@ -556,8 +556,8 @@ var db_functions = {
     },
 
     addFacebookRequest:function (link, response, callback) {
-        var request_ids =response.request;
-        var to  =response.to;
+        var request_ids =response ? response.request :null;
+        var to  =response ? response.to :null;
         db_functions.loggedInAjax({
             url:'/api/fb_request/',
             type:"POST",