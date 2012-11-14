ed2ac2ca511f391da32c8af294bef3ef9141d6e1
Refael Ackermann <refack@gmail.com>
2012-11-14 16:03:56 +0200

    Remove clutter (& possible memory leak) form /updates

--- deliver/routes/updates/main.js
+++ deliver/routes/updates/main.js
@@ -1,41 +1,21 @@
-var models = require('../../../models'),
-    async = require('async');
+var models = require('../../../models')
 
-module.exports = function(req, res){
-
-
-    async.waterfall([
-        function(cbk){
-            models.Update.findById(req.params[0])
-                .select({
-                    '_id':1,
-                    'title':1,
-                    'tooltip':1,
-                    'text_field':1,
-                    'image_field':1,
-                    'tags':1,
-                    'creation_date':1,
-                    'cycle': 1
-                })
-                .populate('cycle', {'_id': 1, 'title': 1})
-                .exec(cbk);
-        }
-    ], function(err, update){
-        if(err)
+module.exports = function(req, res) {
+    models.Update.findById(req.params[0]).populate('cycle', {'_id': 1, 'title': 1}).exec(function(err, update) {
+        if(err) {
             res.render('500.ejs',{error:err});
-        else {
-            res.render('update.ejs',{
-                update: update,
-                meta:{
-                    type:'updates',
-                    title:update.title,
-                    description:update.text_field,
-                    image: (update.image_field_preview && item.image_field.url),
-                    link:'/updates/' + update.id,
-                    id:update.id,
-
-                }
-            });
+            return;
         }
-    })
+        res.render('update.ejs',{
+            update: update,
+            meta:{
+                type:'updates',
+                title:update.title,
+                description:update.text_field,
+                image: (update.image_field_preview && item.image_field.url),
+                link:'/updates/' + update.id,
+                id:update.id
+            }
+        });
+    });
 };
