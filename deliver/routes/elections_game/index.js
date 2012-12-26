6fdc045e6795b2d5660447c5b93bb57c7d1185b9
Lior Ur <liorur@gmail.com>
2012-12-26 14:37:11 +0200

    elections share image

--- deliver/routes/elections_game/index.js
+++ deliver/routes/elections_game/index.js
@@ -5,4 +5,5 @@ module.exports = function(router)
     router.all(/\/([0-9a-f]+)\/?$/,require('./results'));
     router.all('', require('./main'));
     router.get('/results',require('./results'));
+    router.get('/image',require('./image'));
 };
