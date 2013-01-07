feed8a1401803de7bee4a0328bbdf207babe5b90
Lior Ur <liorur@gmail.com>
2013-01-07 12:19:51 +0200

    faces

--- deliver/routes/navigation/faces_game_upload.js
+++ deliver/routes/navigation/faces_game_upload.js
@@ -9,32 +9,6 @@ module.exports = function(req, res){
     console.log(req.files)      ;
     var name= req.files.Filedata.name
 
-    /*
-    fs.readFile(req.files.Filedata.path, function (err, data) {
-        var newPath = 'deliver/public/faces_game/uploads/'+name ;
-        fs.writeFile(newPath, data, function (err) {
-
-            if(err)
-            {
-                console.log('error :' + err )      ;
-                for(var propertyName in err) {
-
-                    console.log(propertyName + ": "+ err[propertyName])  ;
-                }
-
-                res.write(err);
-                res.end();
-            }
-            else {
-                console.log('success :' +'http://www.uru.org.il/faces_game/uploads/'+name)
-                res.write('http://www.uru.org.il/faces_game/uploads/'+name);
-                res.end();
-            }
-        });
-    });
-
-     */
-
 
     fu(req,function(err,value) {
         console.log(value)      ;
@@ -50,8 +24,8 @@ module.exports = function(req, res){
             res.end();
         }
         else {
-            console.log('success :' +'http://www.uru.org.il/faces_game/uploads/'+name)
-            res.write('http://www.uru.org.il/faces_game/uploads/'+name);
+            console.log('success :' +value.url)
+            res.write(value.url);
             res.end();
         }
     });
@@ -64,11 +38,6 @@ function fu(req,callback) {
     function writeToFile(fName, stream, callback){
         var filename=  fName,
             mypath =   'deliver/public/faces_game/uploads/'+fName ;// path.join(__dirname,'..','deliver','public','cdn', fName);
-
-
-
-
-
 
         os= fs.createWriteStream(mypath) ;
             stream.on('data',function(data) {
@@ -111,8 +80,8 @@ function fu(req,callback) {
                 var value_full_path = value.fullPath;
 
                 stream = fs.createReadStream(value.fullPath);
+                knoxClient.putStream(stream, '/fg/'+value.filename , function(err, res){
 
-                knoxClient.putStream(stream, '/' + value.filename + '_' + (new Date().getTime()), function(err, res){
                     if(err)
                         callback(err);
                     else {
@@ -143,42 +112,5 @@ function fu(req,callback) {
         });
     }
 };
-  /*
-      fs.readFile(req.files.upload_file.path, function (err, data) {
-          var newPath = 'deliver/public/faces_game/uploads/'+name ;
-          fs.writeFile(newPath, data, function (err) {
-              if(err) {
-                  res.write(err);
-                  res.end();
-              }else {
-                  res.write('http://www.uru.org.il/faces_game/uploads/'+name);
-                  res.end();
-              }
-          });
-      });
 
-
-
-      */
-/*
-<?php
-
-// default path for the image to be stored //
-$default_path = 'uploads/';
-
-// check to see if a path was sent in from flash //
-$target_path = ($_POST['dir']) ? $_POST['dir'] : $default_path;
-if (!file_exists($target_path)) mkdir($target_path, 0777, true);
-
-// full path to the saved image including filename //
-$destination = $target_path . basename( $_FILES[ 'Filedata' ][ 'name' ] );
-
-// move the image into the specified directory //
-if (move_uploaded_file($_FILES[ 'Filedata' ][ 'tmp_name' ], $destination)) {
-    echo basename( $_FILES[ 'Filedata' ][ 'name' ] );
-} else {
-    echo "FILE UPLOAD FAILED";
-}
-?>
-  */
 
