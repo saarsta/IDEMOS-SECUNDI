63b38a37c7a81cfe430ef82393f9eac35979d829
Lior Ur <liorur@gmail.com>
2013-01-13 15:08:23 +0200

    elections game share image

---
+++ script/eg_stat.js
@@ -0,0 +1,14 @@
+
+var stat=new Array(200);
+db.quotegamegames.find().forEach(function(can){
+
+    if  (!stat[can.quote_count]) {
+        stat[can.quote_count]=0
+    }
+    stat[can.quote_count]++;
+})
+
+for(var i=0; i<stat.length; i++){
+   if(stat[i]) print(i+' - '+stat[i]);
+};
+
