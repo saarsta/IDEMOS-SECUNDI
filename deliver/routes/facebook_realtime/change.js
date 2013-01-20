d02999fcedeebd1de551184b980f3d78a0f8c483
Lior Ur <liorur@gmail.com>
2013-01-20 17:46:42 +0200

    facebook realtime

--- deliver/routes/facebook_realtime/change.js
+++ deliver/routes/facebook_realtime/change.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res){
     console.log('*************** facebook change ***********');
     console.log(req.body);
+    console.log(req.body.entry[0].changed_fields);
     console.log('*******************************************');
     res.write('');
     res.end();
