7f6fcd3661f9bcd6233ec25c9e5de6310cef5b08
Lior Ur <liorur@gmail.com>
2013-01-30 15:58:24 +0200

    homepage smallgov

--- deliver/public/js/common.js
+++ deliver/public/js/common.js
@@ -717,3 +717,14 @@ var getUserPosition = function(user_position, cbk){
     }
     cbk(user_position);
 }
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateName(name) {
+    var re = /^\S{2,}[ ]+\S{2,}$/;
+    var b=re.test(name)
+    return b;
+}
