7b18ce4fa75dc209dce6d61003554bd0a2051222
Lior Ur <liorur@gmail.com>
2013-02-03 13:22:03 +0200

    cycles embed

--- models/cycle.js
+++ models/cycle.js
@@ -59,7 +59,7 @@ var Cycle = module.exports = new Schema({
     social_popup_title: {type: String},
     social_popup_text: {type: String},
     is_hidden:{type:Boolean,'default':true}    ,
-    timeline_embed: {type:mongoose_types.Html}
+    timeline_embed: {type:mongoose_types.Text}
 
 }, {strict: true});
 
