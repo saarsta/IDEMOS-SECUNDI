673c922e47ca98dda4727255b3fda228272aa332
Lior Ur <liorur@gmail.com>
2013-02-13 13:06:09 +0200

    update issue

--- deliver/routes/updates/main.js
+++ deliver/routes/updates/main.js
@@ -2,7 +2,7 @@ var models = require('../../../models')
 
 module.exports = function(req, res) {
     models.Update.findById(req.params[0]).populate('cycle', {'_id': 1, 'title': 1}).exec(function(err, update) {
-        if(err) {
+        if(err || !update) {
             res.render('500.ejs',{error:err});
             return;
         }
