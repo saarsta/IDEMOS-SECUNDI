c6396891325112ca262d8db728524687d808aa50
saarsta <saar@empeeric.com>
2013-02-19 17:35:01 +0200

    fix mail_settings css/style.css remove default "true" from user discussions and cycles mail settings

--- api/user_mail_notification_config_resource.js
+++ api/user_mail_notification_config_resource.js
@@ -4,18 +4,21 @@ var jest = require('jest'),
     _ = require('underscore');
 
 var UserMailNotificationConfig = module.exports = jest.MongooseResource.extend({
-    init: function(){
+    init:function () {
         this._super(models.User, null);
-        this.fields = _.extend({mail_notification_configuration: null},common.user_public_fields);
-        this.update_fields = {mail_notification_configuration:null};
+        this.fields = _.extend({mail_notification_configuration:null, discussions:null}, common.user_public_fields);
+        this.update_fields = {discussions:null, mail_notification_configuration:null};
         this.allowed_methods = ['get', 'put'];
     },
 
-    update_obj:function(req,object,callback){
-        var mail_notification_configuration = req.body;
+    update_obj:function (req, object, callback) {
+        var mail_settings = req.body;
+
         // i'm implementing it because of a terrible bug i can't understand in mongoose, that's why i'm using mongo
-        models.User.update({_id:object._id},{$set: mail_notification_configuration},function(err){
-            callback(err,{});
+
+        // update user
+        models.User.update({_id:object.id}, {$set:mail_settings}, function (err) {
+                callback(err, {});
         });
     }
 })
