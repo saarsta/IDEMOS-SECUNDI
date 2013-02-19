6caeb3a8cf780041d652cdfb241892f82e29ca9c
Lior Ur <liorur@gmail.com>
2013-02-19 15:47:59 +0200

    timeline fix

--- deliver/routes/cycles/main.js
+++ deliver/routes/cycles/main.js
@@ -117,8 +117,8 @@ module.exports = function(req, res){
             var description = g_cycle.text_field_preview || g_cycle.text_field;
             var no_tags_description = description.replace(/(<([^>]+?)>)/ig,"");
 
-            var view =   g_cycle.id==   '5047023a9e56a502000014f5'?   'cycle_new.ejs' : 'cycle.ejs'  ;
-             console.log(view)  ;
+           // var view =   g_cycle.id==   '5047023a9e56a502000014f5'?   'cycle_new.ejs' : 'cycle.ejs'  ;
+           //  console.log(view)  ;
             res.render(view,{
                 cycle: g_cycle,
                 tab:'cycles',
