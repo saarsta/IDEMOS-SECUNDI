90718f667b2df6c13b0a0c2138bf5627d3fe18e4
saarsta <saarsta@gmail.com>
2013-05-06 12:18:06 +0300

    add grunt to heroku buildpath

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,8 +76,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+
     //default task
-    grunt.registerTask('default', 'concat uglify');
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+    grunt.registerTask('heroku', ['concat', 'uglify']);
 
 };
 
