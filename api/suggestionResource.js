b092f61bb093a370d17262551e6d41b2d407f6c5
saarsta <saarsta@gmail.com>
2013-05-22 12:05:00 +0300

    add is_my_suggestion flag

--- api/suggestionResource.js
+++ api/suggestionResource.js
@@ -49,7 +49,8 @@ var SuggestionResource = module.exports = common.GamificationMongooseResource.ex
             },
             wanted_amount_of_tokens:null,
             curr_amount_of_tokens:null,
-            is_editable: null
+            is_editable: null,
+            is_my_suggestion: null
         };
     },
 
@@ -61,6 +62,9 @@ var SuggestionResource = module.exports = common.GamificationMongooseResource.ex
         var user_id = req.user && req.user._id + "";
 
         var iterator = function (suggestion, itr_cbk) {
+            //set is_my_suggestion flag
+            suggestion.is_my_suggestion = (user_id === suggestion.creator_id.id);
+
             // set is_editable flag if user is the creator and its 15 min after publish
             if (user_id === suggestion.creator_id.id && new Date() - suggestion.creation_date <= EDIT_TEXT_LEGIT_TIME){
                 suggestion.is_editable = true;
