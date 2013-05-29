1f6fbb7429b60177175e299630061afd50a7f17f
saarsta <saarsta@gmail.com>
2013-05-29 15:06:43 +0300

    handle threshold Infinity case

--- api/suggestionResource.js
+++ api/suggestionResource.js
@@ -82,8 +82,10 @@ var SuggestionResource = module.exports = common.GamificationMongooseResource.ex
             //wanted amount of tokens is either what admin has entered to the specific suggestion, or the discussion threshold...
             if (suggestion.admin_threshold_for_accepting_the_suggestion > 0)
                 suggestion.wanted_amount_of_tokens = suggestion.admin_threshold_for_accepting_the_suggestion;
-            else
+            else{
+                if (Number(suggestion.threshold_for_accepting_the_suggestion) === Infinity) suggestion.threshold_for_accepting_the_suggestion = null;
                 suggestion.wanted_amount_of_tokens = Number(suggestion.threshold_for_accepting_the_suggestion) || calculate_sugg_threshold(suggestion.getCharCount(), discussion_threshold);
+            }
             if (req.user) {
                 models.GradeSuggestion.findOne({user_id:req.user._id, suggestion_id:suggestion._id + ""}, {"_id":1, "evaluation_grade":1, "does_support_the_suggestion":1}, function (err, grade_sugg_obj) {
                     if (!err && grade_sugg_obj) {
