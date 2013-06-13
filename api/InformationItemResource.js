a9c760eeaa5398e98bd8fdd0e36657d8593ea3ed
saarsta <saarsta@gmail.com>
2013-06-13 16:24:20 +0300

    fix mail body

--- api/InformationItemResource.js
+++ api/InformationItemResource.js
@@ -93,10 +93,10 @@ var InformationItemResource = module.exports = common.GamificationMongooseResour
         var user_id = req.session.user_id;
         var self = this;
         var info_item_object = new self.model();
-        var mail_to = 'saar@uru.org.il';
+        var mail_to = 'aharon@uru.org.il';
         var mail_subject = 'new information item';
         var mail_body =
-            'YO! a new information item is waiting for you.. the title is: ' + '<a href="http://uru-admin.herokuapp.com/admin/model/InformationItem/document/' + fields.id + '">' + fields.title + '</a>';
+            'YO! a new information item is waiting for you.. the title is: ' + '<a href="http://uru-admin.herokuapp.com/admin/model/InformationItem/document/' + info_item_object.id + '">' + fields.title + '</a>';
 
 
         fields.created_by = {creator_id:user_id, did_user_created_this_item: true};
